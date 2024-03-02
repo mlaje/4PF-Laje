79c72d6cb58bb2dd29625a383df7de1ca1e68ebe
Marcelo Laje <marcelo.laje@skycop.com.ar>
2024-03-02 17:57:54 -0300

    Sites dialog

--- src/app/layouts/dashboard/pages/sites/components/sites-dialog/sites-dialog.component.ts
+++ src/app/layouts/dashboard/pages/sites/components/sites-dialog/sites-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { SitesActions } from '../../store/sites.actions';
 
 @Component({
   selector: 'app-sites-dialog',
@@ -6,5 +10,37 @@ import { Component } from '@angular/core';
   styleUrl: './sites-dialog.component.scss'
 })
 export class SitesDialogComponent {
+
+  
+  siteForm: FormGroup;
+
+  constructor(
+              private store: Store,
+              private formBuilder: FormBuilder,
+              private matDialogRef: MatDialogRef<SitesDialogComponent>) {
+      
+      this.siteForm = this.formBuilder.group({
+            nombre: this.formBuilder.control(''),
+            calle: this.formBuilder.control(''),
+            altura: this.formBuilder.control(''),
+            codigoPostal: this.formBuilder.control(''),
+            aulas: this.formBuilder.control(''),
+    });
+
+  }  
+
+  onSubmit(): void {
+    if (this.siteForm.invalid) {
+      this.siteForm.markAllAsTouched();
+    } else {
+      this.store.dispatch(
+        SitesActions.createSite({ data: this.siteForm.value })
+      );
+      this.matDialogRef.close();
+    }
+  }
+
+
+
 
 }

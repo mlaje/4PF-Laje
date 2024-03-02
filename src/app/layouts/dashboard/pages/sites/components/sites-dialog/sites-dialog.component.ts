7c294aa229da9e7d51162ff553833cda2479668c
Marcelo Laje <marcelo.laje@skycop.com.ar>
2024-03-02 18:39:48 -0300

    Mejoras Inscripciones y Sedes

--- src/app/layouts/dashboard/pages/sites/components/sites-dialog/sites-dialog.component.ts
+++ src/app/layouts/dashboard/pages/sites/components/sites-dialog/sites-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { SitesActions } from '../../store/sites.actions';
@@ -20,11 +20,11 @@ export class SitesDialogComponent {
               private matDialogRef: MatDialogRef<SitesDialogComponent>) {
       
       this.siteForm = this.formBuilder.group({
-            nombre: this.formBuilder.control(''),
-            calle: this.formBuilder.control(''),
-            altura: this.formBuilder.control(''),
-            codigoPostal: this.formBuilder.control(''),
-            aulas: this.formBuilder.control(''),
+            nombre: this.formBuilder.control('', [Validators.required, Validators.minLength(3) ]),
+            calle: this.formBuilder.control('', [Validators.required, Validators.minLength(1) ]),
+            altura: this.formBuilder.control('', [Validators.required]),
+            codigoPostal: this.formBuilder.control('', [Validators.required]),
+            aulas: this.formBuilder.control('', [Validators.required])
     });
 
   }  
